792cd4a54d9b45ad139668dc0b6263982aa3cffd
Timo Witte <timo.witte@googlemail.com>
2013-08-14 19:18:21 +0200

    - fixed formating, added last shader, removed some warning with gcc - texture is now created as big as the received camera image

--- FPV.cpp
+++ FPV.cpp
@@ -11,7 +11,6 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
-
 /* ASCII code for the escape key. */
 #define ESCAPE 27
 
@@ -27,78 +26,73 @@ void ReSizeGLScene(int Width, int Height);
 
 GLuint shaderProgram, vertexShader, fragmentShader;
 
-int main(int argc, char **argv) 
-{  
-  glutInit(&argc, argv);  
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);  
 
-  /* Select type of Display mode:   
-     Double buffer 
-     RGBA color
-     Alpha components supported 
-     Depth buffer */  
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);  
+	/* Select type of Display mode:   
+	Double buffer 
+	RGBA color
+	Alpha components supported 
+	Depth buffer */  
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);  
 
-  /* get a 640 x 480 window */
-  glutInitWindowSize(640, 480);  
+	/* get a 640 x 480 window */
+	glutInitWindowSize(640, 480);  
 
-  /* the window starts at the upper left corner of the screen */
-  glutInitWindowPosition(0, 0);  
+	/* the window starts at the upper left corner of the screen */
+	glutInitWindowPosition(0, 0);  
 
-  /* Open a window */  
-  window = glutCreateWindow("FPV Viewer");  
+	/* Open a window */  
+	window = glutCreateWindow("FPV Viewer");  
 
-  /* Register the function to do all our OpenGL drawing. */
-  glutDisplayFunc(&DrawGLScene);  
+	/* Register the function to do all our OpenGL drawing. */
+	glutDisplayFunc(&DrawGLScene);  
 
-  /* Go fullscreen.  This is the soonest we could possibly go fullscreen. */
-  // glutFullScreen();
+	/* Go fullscreen.  This is the soonest we could possibly go fullscreen. */
+	// glutFullScreen();
 
-  /* Even if there are no events, redraw our gl scene. */
-  glutIdleFunc(&DrawGLScene);
+	/* Even if there are no events, redraw our gl scene. */
+	glutIdleFunc(&DrawGLScene);
 
-  /* Register the function called when our window is resized. */
-  glutReshapeFunc(&ReSizeGLScene);
+	/* Register the function called when our window is resized. */
+	glutReshapeFunc(&ReSizeGLScene);
 
-  /* Register the function called when the keyboard is pressed. */
-  glutKeyboardFunc(&keyPressed);
+	/* Register the function called when the keyboard is pressed. */
+	glutKeyboardFunc(&keyPressed);
 
-  /* Initialize our window. */
-  InitGL(640, 480);
-  
-  /* Start Event Processing Engine */  
-  glutMainLoop();  
+	/* Initialize our window. */
+	InitGL(640, 480);
 
-  return 1;
+	/* Start Event Processing Engine */  
+	glutMainLoop();  
+
+	return 1;
 }
 
-std::string get_file_contents(const char *filename)
-{
-  std::ifstream in(filename, std::ios::in | std::ios::binary);
-  if (in)
-  {
-    std::string contents;
-    in.seekg(0, std::ios::end);
-    contents.resize(in.tellg());
-    in.seekg(0, std::ios::beg);
-    in.read(&contents[0], contents.size());
-    in.close();
-    return(contents);
-  }
-  // throw(errno);
+std::string get_file_contents(const char *filename) {
+	std::ifstream in(filename, std::ios::in | std::ios::binary);
+	if (in) {
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	else
+		throw(123);
 }
 
 /* A general OpenGL initialization function.  Sets all of the initial parameters. */
-void InitGL(int Width, int Height)	        // We call this right after our OpenGL window is created.
-{
+// We call this right after our OpenGL window is created.
+void InitGL(int Width, int Height) {
 	unsigned char data[720*480*3];
-	int c = 0;
 	
 	unsigned int textureID;
 	char log[2000];
 
 	std::string VShaderSrc = get_file_contents("vshader.txt");
-	const char* VShaderString = VShaderSrc.c_str();
-	int VShaderLength = VShaderSrc.length();
 
 	std::string FShaderSrc = get_file_contents("fshader.txt");
 	const char* FShaderString = FShaderSrc.c_str();
@@ -123,15 +117,17 @@ void InitGL(int Width, int Height)	        // We call this right after our OpenG
 
 
 	// create a texture for the image...
-	glTexImage2D(GL_TEXTURE_2D,            	 //target
-				 0,                         //level of detail
-				 3,                         //colour components
-				 720,                       //width
-				 480,                       //height
-				 0,                         //border
-				 GL_RGB,                    //image format
-				 GL_UNSIGNED_BYTE,
-				 data);
+	glTexImage2D(
+		GL_TEXTURE_2D,            	 //target
+		0,                         //level of detail
+		3,                         //colour components
+		720,                       //width
+		480,                       //height
+		0,                         //border
+		GL_RGB,                    //image format
+		GL_UNSIGNED_BYTE,
+		data
+	);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
@@ -163,20 +159,19 @@ void InitGL(int Width, int Height)	        // We call this right after our OpenG
 }
 
 /* The function called when our window is resized (which shouldn't happen, because we're fullscreen) */
-void ReSizeGLScene(int Width, int Height)
-{
-  if (Height==0)				// Prevent A Divide By Zero If The Window Is Too Small
-    Height=1;
+void ReSizeGLScene(int Width, int Height) {
+	if (Height==0)				// Prevent A Divide By Zero If The Window Is Too Small
+		Height=1;
 
-  glViewport(0, 0, Width, Height);		// Reset The Current Viewport And Perspective Transformation
+	glViewport(0, 0, Width, Height);		// Reset The Current Viewport And Perspective Transformation
 
-  glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);
 
 
 	GLint loc = glGetUniformLocation(shaderProgram, "resolution");
 	if (loc != -1)
 	{
-	   glUniform2f(loc, Width, Height);
+		glUniform2f(loc, Width, Height);
 	}
 }
 
@@ -184,38 +179,31 @@ void ReSizeGLScene(int Width, int Height)
 IplImage* frame;
 
 /* The main drawing function. */
-void DrawGLScene()
-{
+void DrawGLScene() {
+	frame = cvQueryFrame( capture );
 
-	/*
-	int c = 0;
-	for(c = 0; c < 640*480; c++) {
-		data[c] = (unsigned char)rand();
+	if ( !frame ) {
+		fprintf( stderr, "ERROR: frame is null...\n" );
 	}
-	*/
 
-    frame = cvQueryFrame( capture );
+	// create the openGL Texture from the imagedata
+	glTexSubImage2D(GL_TEXTURE_2D,
+		0,
+		0,
+		0,
+		frame->width,
+		frame->height,
+		GL_BGR_EXT,
+		GL_UNSIGNED_BYTE,
+		frame->imageData
+	);
 
-     if ( !frame ) {
-       fprintf( stderr, "ERROR: frame is null...\n" );
-     }
+	// glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
+	// glMatrixMode(GL_MODELVIEW);
+	// glLoadIdentity();
 
-  glTexSubImage2D(GL_TEXTURE_2D,
-	                0,
-			0,
-			0,
-			720,
-			480,
-			GL_BGR_EXT,
-			GL_UNSIGNED_BYTE,
-			frame->imageData);
-
-  // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
-
-  // glMatrixMode(GL_MODELVIEW);
-
-  // glLoadIdentity();
-  glBegin(GL_QUADS);
+	// draw a rectangle with the image data inside
+	glBegin(GL_QUADS);
 	glTexCoord2f(1,1);
 	glVertex2f(-1,-1);
 
@@ -227,29 +215,24 @@ void DrawGLScene()
 
 	glTexCoord2f(1,0);
 	glVertex2f(-1,1);
-  glEnd();
+	glEnd();
 
-  // glFlush();
-  glutSwapBuffers();
-
+	// glFlush();
+	glutSwapBuffers();
 }
 
 /* The function called whenever a key is pressed. */
-void keyPressed(unsigned char key, int x, int y) 
-{
-    /* avoid thrashing this procedure */
-    // usleep(100);
-
-    /* If escape is pressed, kill everything. */
-    if (key == ESCAPE) 
-    { 
-	/* shut down our window */
-	glutDestroyWindow(window); 
+void keyPressed(unsigned char key, int x, int y) {
+	/* If escape is pressed, kill everything. */
+	if (key == ESCAPE)  { 
+		/* shut down our window */
+		glutDestroyWindow(window); 
 	
-	/* exit the program...normal termination. */
-	exit(0);                   
-    }
+		/* exit the program...normal termination. */
+		exit(0);                   
+	}
 
+	// if f is pressed go fullscreen
 	if(key == 'f') {
 		glutFullScreen();
 	}
--- Makefile
+++ Makefile
@@ -6,7 +6,11 @@ CC = g++
 CFLAGS = $(COMPILERFLAGS) $(INCLUDE)
 LIBRARIES = -lX11 -lXi -lXmu -lglut -lGL -lGLU -lm -lopencv_calib3d -lopencv_contrib -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_highgui -lopencv_imgproc -lopencv_legacy -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_stitching -lopencv_ts -lopencv_video -lopencv_videostab -lGLEW
 
-all: $(basename $(wildcard FPV.cpp))
-
 fpv : FPV.o
-	$(CC) $(CFLAGS) -o $@ $(LIBDIR) $< $(LIBRARIES)
+	$(CC) FPV.o -o fpv $(LIBDIR) $(LIBRARIES)
+
+FPV.o : FPV.cpp
+	$(CC) $(CFLAGS) -c FPV.cpp -o FPV.o
+
+clean :
+	rm FPV.o fpv
